4f9584685177ed96c9d50b009db8bf68309b9e74
Elias Rahmani <eliass.rahmani@gmail.com>
2022-07-13 00:15:11 +0430

    add argument parsing

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <strings.h>
+#include <string.h>
 #include "LinkedList/LinkedList.h"
 
 int main(int argc, char *argv[])
 {
     list_t *pr = (list_t *)malloc(sizeof(list_t));
-    puts("Enter a number: ");
-    int loop = 10;
-    scanf("%d", &loop);
+    int loop;
+    int show_len = 0;
+    int show_prime = 0;
+    int i = 1;
+    while(i < argc) {
+        if(!strncasecmp(argv[i], "-n", 2)) {
+            loop = (int)strtol(argv[++i], NULL, 10);
+        }
+        else if(!strncasecmp(argv[i], "-l",2)) {
+            show_len = 1;
+        }
+        else if(!strncasecmp(argv[i], "-p", 9)) {
+            show_prime = 1;
+        }
+        else {
+            fprintf(stderr, "Unknown argument: %s\n", argv[i]);
+        }
+        i++;
+    }
+    if(loop == 0) {
+        fprintf(stderr, "No loop number specified. use -n to specify a loop number. like -n 26\n");
+        return 1;
+    }
     prime(loop, pr);
-    printf("Prime numbers: \n");
-    print_list(pr);
-    printf("Number of primes = %d\n", len(pr));
+    if(show_prime) {
+        printf("Prime numbers: \n");
+        print_list(pr);
+    }
+    if(show_len) {
+        printf("Number of primes = %d\n", len(pr));
+    }
     free(pr);
     return 0;
 }
