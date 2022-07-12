7c87976e8669e7ed96c6d7242b82cd36bb29b58b
Elias Rahmani <eliass.rahmani@gmail.com>
2022-07-12 23:43:30 +0430

    add length of list

--- main.c
+++ main.c
@@ -9,7 +9,9 @@ int main(int argc, char *argv[])
     int loop = 10;
     scanf("%d", &loop);
     prime(loop, pr);
+    printf("Prime numbers: \n");
     print_list(pr);
+    printf("Number of primes = %d\n", len(pr));
     free(pr);
     return 0;
 }
